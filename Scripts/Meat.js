072a0179c01af238f40a89c3a88a2b9c9a1ddf0f
lesajayaweera <166988125+lesajayaweera@users.noreply.github.com>
2024-08-12 15:46:44 +0530

    added meat items to the product array

---
+++ Scripts/Meat.js
@@ -0,0 +1,28 @@
+import { products } from "./data/Products.js";
+
+const Meat = products[1];
+
+let html = '';
+
+Meat.forEach((item) => {
+     html += `
+        <div class="product-container">
+            <div class="product-image-container">
+                <img class="product-image" src="${item.Image}" alt="${item.Image}">
+            </div>
+            <div class="product-name-container">
+                <p class="product-name">${item.name}</p>
+                <p>${item.unit}</p>
+                <p>Rs.${(item.price / 100).toFixed(2)}</p>
+            </div>
+            <div class="add-to-cart-button-container">
+                <button class="add-to-cart-button" data-product-id="${item.id}">
+                    ADD TO CART
+                </button>
+            </div>
+        
+        </div>
+        `;
+})
+
+document.getElementById("product-main-container").innerHTML = html;;
--- Scripts/Vegetables.js
+++ Scripts/Vegetables.js
@@ -1,31 +1,29 @@
 import { products } from "./data/Products.js";
 
 let html ='';
-products.forEach((cateogory)=>{
-    cateogory.forEach((item)=>{
-        console.log(item.name);
-        console.log(item.Image);
-        html += `
+const vegetables = products[0];
+
+vegetables.forEach((item)=>{
+    html += `
         <div class="product-container">
             <div class="product-image-container">
-                <img class="product-image" src="${item.Image}" alt="image1">
+                <img class="product-image" src="${item.Image}" alt="${item.Image}">
             </div>
             <div class="product-name-container">
                 <p class="product-name">${item.name}</p>
                 <p>${item.unit}</p>
-                <p>Rs.${(item.price/100).toFixed(2)}</p>
+                <p>Rs.${(item.price / 100).toFixed(2)}</p>
             </div>
             <div class="add-to-cart-button-container">
-                <button class="add-to-cart-button">
+                <button class="add-to-cart-button" data-product-id="${item.id}">
                     ADD TO CART
                 </button>
             </div>
         
         </div>
         `;
-        
-
-    })
 })
-
+const buttons = document.querySelectorAll(".add-to-cart-button");
 document.getElementById("product-main-container").innerHTML =html;
+console.log(buttons.dataset.productId);
+
