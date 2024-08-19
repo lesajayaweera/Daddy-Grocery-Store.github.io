db5eda6d64f556f72f7c63f2ea9b85c782afbe56
lesajayaweera <166988125+lesajayaweera@users.noreply.github.com>
2024-08-19 18:48:45 +0530

    cart half done up to the calculate no. of items and  display items from the cart and show the total cost of the cost of the cart

--- Scripts/cart.js
+++ Scripts/cart.js
@@ -1,3 +1,4 @@
+
 let cart = JSON.parse(localStorage.getItem("cart"));
 console.log(cart);
 
@@ -36,50 +37,7 @@ cart.forEach((product)=>{
                 </div>
               </div>
 
-              <div class="delivery-options">
-                <div class="delivery-options-title">
-                  Choose a delivery option:
-                </div>
-                <div class="delivery-option">
-                  <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-1">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-              </div>
+              
             </div>
           </div>
         `;
@@ -89,8 +47,23 @@ cart.forEach((product)=>{
 })
 document.querySelector(".order-summary").innerHTML = html;
 
-document.getElementById("checkoutCount").textContent = `${cart.length} items`;
- 
+const productCounter= document.querySelectorAll("#checkoutCount");
+productCounter.forEach((container)=>{
+  container.textContent = `${cart.length} items`;
+  container.style.color = "rgb(196, 80, 0)";
+  container.style.fontWeight = "600"
+})
+
+function CalculateCost(){
+  const costDisplay = document.querySelector(".payment-summary-money-cost");
+  let totalCost = 0;
+  cart.forEach((product) => {
+    totalCost += product.price * product.quantity;
+  });
+  costDisplay.textContent = `Rs.${(totalCost / 100).toFixed(2)}`;
+}
+
+CalculateCost();
   
 
 
